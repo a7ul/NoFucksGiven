3a4c5c95c023b88640b230ca0c1a6d30125f44dd
Atul <atulanand94@gmail.com>
2016-07-05 00:07:39 +0530

    Database handler

--- app/src/main/java/rationalduos/atulsoori/nofucksgiven/LoadScreen.java
+++ app/src/main/java/rationalduos/atulsoori/nofucksgiven/LoadScreen.java
@@ -1,5 +1,6 @@
 package rationalduos.atulsoori.nofucksgiven;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -17,16 +18,25 @@ import java.io.InputStream;
 import java.net.URL;
 
 import rationalduos.atulsoori.nofucksgiven.utils.AppConstants;
+import rationalduos.atulsoori.nofucksgiven.utils.DatabaseHandler;
 import rationalduos.atulsoori.nofucksgiven.utils.JsonReader;
 
 public class LoadScreen extends AppCompatActivity {
+    private DatabaseHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_screen);
+
+        dbHandler = new DatabaseHandler(this);
+
         Thread loader = new Thread(new LoadRunner(this));
         loader.start();
+    }
+
+    public DatabaseHandler getDbHandler() {
+        return this.dbHandler;
     }
 }
 
@@ -41,24 +51,24 @@ class LoadRunner implements Runnable {
     public void run() {
         try {
             // Get index json
-            JSONObject indexJson = new JsonReader(AppConstants.SERVER_URL+AppConstants.INDEX_FILE).getJson();
-            downloadFile(AppConstants.SERVER_URL+AppConstants.INDEX_FILE,AppConstants.INDEX_FILE); // added by atul
+            JSONObject indexJson = new JsonReader(AppConstants.SERVER_URL + AppConstants.INDEX_FILE).getJson();
+            downloadFile(AppConstants.SERVER_URL + AppConstants.INDEX_FILE, AppConstants.INDEX_FILE); // added by atul
             SharedPreferences settings = activity.getSharedPreferences(AppConstants.PREF_NAME, 0);
 
             Boolean isAppInitialized = settings.getBoolean(AppConstants.PREF_INITIALIZED, false);
-            Log.d("Load screen",indexJson.toString());
-            verifyConfigs(indexJson,settings);
+            Log.d("NFG", indexJson.toString());
+            verifyConfigs(indexJson, settings);
             Intent i = new Intent(activity.getApplicationContext(), MainActivity.class);
             activity.startActivity(i);
             activity.finish();
         } catch (IOException e) {
             //TODO
             // No internet connection?
-            Log.e("Load screen",e.getMessage());
+            Log.e("NFG", e.getMessage());
         } catch (JSONException e) {
             //TODO
             // What's this?
-            Log.e("Load screen",e.getMessage());
+            Log.e("NFG", e.getMessage());
         }
     }
 
@@ -67,15 +77,15 @@ class LoadRunner implements Runnable {
         JSONArray configsArray = indexJson.getJSONArray(AppConstants.INDEX_CONFIGS);
         SharedPreferences.Editor editor = settings.edit();
 
-        for (int i=0; i < configsArray.length(); i++){
+        for (int i = 0; i < configsArray.length(); i++) {
             JSONObject config = configsArray.getJSONObject(i);
             String config_fname = config.getString(AppConstants.INDEX_CONFIGS_FILE);
             String config_md5 = config.getString(AppConstants.INDEX_CONFIGS_MD5);
-            String cur_md5 = settings.getString(config_fname+"_md5","00");
-            Log.d("Load screen","Config: " + config_fname);
-            Log.d("Load screen","Config md5: " + config_md5);
-            Log.d("Load screen","Current md5: " + cur_md5);
-            if(!cur_md5.equals(config_md5)) {
+            String cur_md5 = settings.getString(config_fname + "_md5", "00");
+            Log.d("NFG", "Config: " + config_fname);
+            Log.d("NFG", "Config md5: " + config_md5);
+            Log.d("NFG", "Current md5: " + cur_md5);
+            if (!cur_md5.equals(config_md5)) {
                 downloadFile(AppConstants.SERVER_URL + basePath + config_fname, config_fname);
                 editor.putString(config_fname + "_md5", config_md5);
             }
@@ -85,29 +95,24 @@ class LoadRunner implements Runnable {
         editor.apply();
 
         //  Following block is just for testing
-        for (int i=0; i < configsArray.length(); i++){
+        for (int i = 0; i < configsArray.length(); i++) {
             JSONObject config = configsArray.getJSONObject(i);
             String config_fname = config.getString(AppConstants.INDEX_CONFIGS_FILE);
-            FileInputStream fis =  activity.openFileInput(config_fname);
+            FileInputStream fis = activity.openFileInput(config_fname);
             String raw = new JsonReader(fis).toString();
-            Log.d("Load screen",config_fname+ " --> " + raw);
+            Log.d("NFG", "CONFIG: " + config_fname + " --> " + raw);
         }
-
     }
 
     private void downloadFile(String url, String fname) throws IOException {
-        Log.d("Load screen","Download file: " + fname);
+        Log.d("NFG", "Download file: " + fname);
         byte[] buffer = new byte[1024];
         int bytesRead;
         InputStream input = new URL(url).openStream();
-        Log.d("Load screen","Download file: " + fname);
-        FileOutputStream output = activity.openFileOutput(fname, activity.MODE_PRIVATE);
-        Log.d("Load screen","Download file: " + fname);
-        while ((bytesRead = input.read(buffer)) != -1 )
-        {
+        FileOutputStream output = activity.openFileOutput(fname, Context.MODE_PRIVATE);
+        while ((bytesRead = input.read(buffer)) != -1) {
             output.write(buffer, 0, bytesRead);
         }
-        Log.d("Load screen","Download file: " + fname);
         output.close();
     }
 }
---
+++ app/src/main/java/rationalduos/atulsoori/nofucksgiven/utils/DatabaseHandler.java
@@ -0,0 +1,185 @@
+package rationalduos.atulsoori.nofucksgiven.utils;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rationalduos.atulsoori.nofucksgiven.models.CardInfo;
+
+/**
+ * Created by atulr on 04/07/16.
+ */
+public class DatabaseHandler extends SQLiteOpenHelper {
+    // Database Version
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "NoFucksGivenDb";
+
+    // Contacts table name
+    private static final String TABLE_FUCKS = "fucks";
+
+    // Contacts Table Columns names
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
+    private static final String KEY_CONTRIBUTOR = "contributor";
+    private static final String KEY_TYPE = "type";
+    private static final String KEY_DATA = "data";
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    // Creating Tables
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_FUCKS_TABLE = "CREATE TABLE " + TABLE_FUCKS + "("
+                + KEY_ID + " TEXT PRIMARY KEY," + KEY_NAME + " TEXT,"
+                + KEY_CONTRIBUTOR + " TEXT" + KEY_TYPE + " TEXT," + KEY_DATA + " TEXT," + ")";
+        db.execSQL(CREATE_FUCKS_TABLE);
+    }
+
+    // Upgrading database
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_FUCKS);
+        // Create tables again
+        onCreate(db);
+    }
+
+
+    // Adding new CardInfo
+    public void addFuck(CardInfo f) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(KEY_ID, f.getId());
+        values.put(KEY_NAME, f.getName());
+        values.put(KEY_CONTRIBUTOR, f.getContributor());
+        values.put(KEY_TYPE, f.getType());
+        values.put(KEY_DATA, f.getData());
+        // Inserting Row
+        db.insert(TABLE_FUCKS, null, values);
+        db.close(); // Closing database connection
+    }
+
+    public void addListOfFucks(List<CardInfo> cardInfoList){
+        SQLiteDatabase db = this.getWritableDatabase();
+        for(CardInfo f: cardInfoList){
+            ContentValues values = new ContentValues();
+            values.put(KEY_ID, f.getId());
+            values.put(KEY_NAME, f.getName());
+            values.put(KEY_CONTRIBUTOR, f.getContributor());
+            values.put(KEY_TYPE, f.getType());
+            values.put(KEY_DATA, f.getData());
+            // Inserting Row
+            db.insert(TABLE_FUCKS, null, values);
+        }
+        db.close(); // Closing database connection
+    }
+
+    // Getting single CardInfo
+    public CardInfo getFuck(String id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_FUCKS,
+                new String[]{KEY_ID, KEY_NAME, KEY_CONTRIBUTOR, KEY_TYPE, KEY_DATA},
+                KEY_ID + "=?",
+                new String[]{id}, null, null, null, null);
+
+        CardInfo f = null;
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            f = new CardInfo(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4));
+
+            cursor.close();
+        }
+        return f;
+    }
+
+    // Getting All CardInfos
+    public List<CardInfo> getAllFucks() {
+        List<CardInfo> cardInfoList = new ArrayList<>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_FUCKS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                CardInfo cardInfo = new CardInfo(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4));
+                // Adding cardInfo to list
+                cardInfoList.add(cardInfo);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return cardInfoList;
+    }
+
+    public List<CardInfo> getAllFucksOfType(String type) {
+        List<CardInfo> cardInfoList = new ArrayList<>();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + TABLE_FUCKS;
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery + " WHERE " + KEY_TYPE + " = ?", new String[]{type});
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                CardInfo cardInfo = new CardInfo(cursor.getString(0), cursor.getString(1), cursor.getString(2), cursor.getString(3), cursor.getString(4));
+                // Adding cardInfo to list
+                cardInfoList.add(cardInfo);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return cardInfoList;
+    }
+
+    // Getting cardInfo Count
+    public int getFucksCount() {
+        String countQuery = "SELECT  * FROM " + TABLE_FUCKS;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+        cursor.close();
+        return cursor.getCount();
+    }
+
+    // Updating single cardInfo
+    public int updateFuck(CardInfo f) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(KEY_NAME, f.getName());
+        values.put(KEY_CONTRIBUTOR, f.getContributor());
+        values.put(KEY_TYPE, f.getType());
+        values.put(KEY_DATA, f.getData());
+
+        // updating row
+        return db.update(TABLE_FUCKS, values, KEY_ID + " = ?",
+                new String[]{f.getId()});
+    }
+
+    // Deleting single cardInfo
+    public void deleteFuck(CardInfo f) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_FUCKS, KEY_ID + " = ?",
+                new String[]{f.getId()});
+        db.close();
+    }
+
+    public void deleteAllFucks(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(TABLE_FUCKS, null, null);
+        db.close();
+    }
+
+}
