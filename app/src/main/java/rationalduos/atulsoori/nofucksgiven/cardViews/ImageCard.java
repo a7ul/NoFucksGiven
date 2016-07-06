c602485af507c452bad0a07bf1af1607664c15c8
Raveendra Soori <sooriravindra@gmail.com>
2016-07-07 04:50:50 +0530

    Share on image now works!

--- app/src/main/java/rationalduos/atulsoori/nofucksgiven/cardViews/ImageCard.java
+++ app/src/main/java/rationalduos/atulsoori/nofucksgiven/cardViews/ImageCard.java
@@ -1,10 +1,17 @@
 package rationalduos.atulsoori.nofucksgiven.cardViews;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +23,15 @@ import android.widget.LinearLayout;
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import rationalduos.atulsoori.nofucksgiven.R;
+import rationalduos.atulsoori.nofucksgiven.utils.AppConstants;
 
 
 /**
@@ -27,6 +40,7 @@ import rationalduos.atulsoori.nofucksgiven.R;
 public class ImageCard extends GenericCard {
     SubsamplingScaleImageView imageView;
     String url;
+    Bitmap imageBitmap;
     private LinearLayout spinner;
     private LinearLayout errorView;
     private Button retryButton;
@@ -39,7 +53,7 @@ public class ImageCard extends GenericCard {
     public View onCreateView(LayoutInflater inflater, ViewGroup vg, Bundle savedInstanceState) {
 
         View view = super.onCreateView(inflater, vg, savedInstanceState);
-
+        imageBitmap = null;
         imageView = (SubsamplingScaleImageView) view.findViewById(R.id.image_card_image);
         spinner = (LinearLayout) view.findViewById(R.id.progressBar);
         errorView = (LinearLayout) view.findViewById(R.id.error_view);
@@ -49,7 +63,7 @@ public class ImageCard extends GenericCard {
             url = getArguments().getString("imageUrl");
         } catch (Exception e) {
             url = "";
-            Log.e("NFG",Log.getStackTraceString(e));
+            Log.e("NFG", Log.getStackTraceString(e));
         }
 
         retryButton.setOnClickListener(new View.OnClickListener() {
@@ -75,14 +89,55 @@ public class ImageCard extends GenericCard {
 
     @Override
     public void shareCardData() {
-        //TODO
+        /*
+        Saves image to internal files dir and use content
+        provider to share image
+        */
+        Context context = getContext();
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("image/jpeg");
+
+        /*
+         Create a dir in app's internal files dir, We will give read permissions on this dir
+         No other files will go here. See manifest for content provider declaration
+         */
+        File shareImageDir = new File(context.getFilesDir(),AppConstants.APP_DIRECTORY);
+        if (!shareImageDir.exists()) {
+            shareImageDir.mkdirs();
+        }
+        File shareImageFile = new File(shareImageDir,AppConstants.TEMP_IMG_FILE);
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        try {
+            shareImageFile.createNewFile();
+            FileOutputStream fo = new FileOutputStream(shareImageFile);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Uri image_uri = FileProvider.getUriForFile(context,"io.github.bucketdevelopers",
+                                                        new File(shareImageDir, AppConstants.TEMP_IMG_FILE));
+        shareIntent.putExtra(Intent.EXTRA_STREAM, image_uri);
+
+        // Get all apps that can recieve our intent and give them permissions
+        List<ResolveInfo> resInfoList = context.getPackageManager()
+                                    .queryIntentActivities(shareIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo resolveInfo : resInfoList) {
+            String packageName = resolveInfo.activityInfo.packageName;
+            context.grantUriPermission(packageName, image_uri, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        startActivity(Intent.createChooser(shareIntent, AppConstants.SHARE_ACTIVITY_NAME));
+        context.revokeUriPermission(image_uri,Intent.FLAG_GRANT_READ_URI_PERMISSION);
     }
 
     private class DownloadImage extends AsyncTask<String, Void, Bitmap> {
         LinearLayout spinner;
-        public DownloadImage(LinearLayout spinner){
+
+        public DownloadImage(LinearLayout spinner) {
             this.spinner = spinner;
         }
+
         @Override
         protected void onPreExecute() {
             super.onPreExecute();
@@ -94,22 +149,21 @@ public class ImageCard extends GenericCard {
         protected Bitmap doInBackground(String... URL) {
 
             String imageURL = URL[0];
-            Bitmap bitmap = null;
             try {
                 InputStream input = new java.net.URL(imageURL).openStream();
-                bitmap = BitmapFactory.decodeStream(input);
+                imageBitmap = BitmapFactory.decodeStream(input);
             } catch (Exception e) {
-                Log.e("NFG",Log.getStackTraceString(e));
+                Log.e("NFG", Log.getStackTraceString(e));
             }
-            return bitmap;
+            return imageBitmap;
         }
 
         @Override
         protected void onPostExecute(Bitmap result) {
-            try{
+            try {
                 imageView.setImage(ImageSource.bitmap(result));
-            }catch (Exception e){
-                Log.e("NFG",Log.getStackTraceString(e));
+            } catch (Exception e) {
+                Log.e("NFG", Log.getStackTraceString(e));
                 errorView.setVisibility(View.VISIBLE);
             }
             spinner.setVisibility(View.GONE);
